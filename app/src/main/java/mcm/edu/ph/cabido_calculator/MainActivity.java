a93c2ede3ad6a49dfe767a7ad38b99f2a77dc214
BeeCabs <beecabido@gmail.com>
2021-01-19 14:45:14 +0800

    Updated buttons Changed button colors

--- app/src/main/java/mcm/edu/ph/cabido_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/cabido_calculator/MainActivity.java
@@ -2,13 +2,40 @@ package mcm.edu.ph.cabido_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnSub,btnDiv,btnMult,btnC,btnEqual,btnChange,btnDecimal,btnMod,textView;
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMult = findViewById(R.id.btnMult);
+        btnC = findViewById(R.id.btnC);
+        btnEqual = findViewById(R.id.btnEqual);
+        btnChange = findViewById(R.id.btnChange);
+        btnDecimal = findViewById(R.id.btnDecimal);
+        btnMod = findViewById(R.id.btnMod);
+        textView = findViewById(R.id.textView);
+
     }
 }
